967b336bb524dc4f3226ff57d0a63fe0a5e8f2a4
Nitin Khaitan <khaitan.nitin@gmail.com>
2024-02-21 00:09:16 +1100

    lambda support for vpc

--- Network_Production/terraform/cloudwatch_log_retention.tf
+++ Network_Production/terraform/cloudwatch_log_retention.tf
@@ -1,15 +1,15 @@
-module "cloudwatch_logs_retention_network_nonproduction001" {
-  source  = "Codzs-Architecture/cloudwatch-log-retention/aws"
-  version = "0.0.3"
+# module "cloudwatch_logs_retention_network_nonproduction001" {
+#   source  = "Codzs-Architecture/cloudwatch-log-retention/aws"
+#   version = "0.0.4"
   
-  retention_days_target = 30
-  retention_days_min    = 30
-  retention_days_max    = 90
-  discover_regions      = "false"
-  # dry_run               = "false"
-  # regex_match           = ""
-  # regex_exclude         = "^$"
-  schedule_expression   = "rate(23 hours)"
-  delete_empty_days     = 600
-  environment           = "prod"
-}
+#   retention_days_target = 30
+#   retention_days_min    = 30
+#   retention_days_max    = 90
+#   discover_regions      = "false"
+#   # dry_run               = "false"
+#   # regex_match           = ""
+#   # regex_exclude         = "^$"
+#   schedule_expression   = "rate(23 hours)"
+#   delete_empty_days     = 600
+#   environment           = "prod"
+# }
--- Network_Production/terraform/vpc.tf
+++ Network_Production/terraform/vpc.tf
@@ -1,6 +1,6 @@
-module "vpc_network_production" {
-  source  = "Codzs-Architecture/vpc-impl/aws"
-  version = "0.0.10"
+# module "vpc_network_production" {
+#   source  = "Codzs-Architecture/vpc-impl/aws"
+#   version = "0.0.11"
 
-  account_customizations_name = "${local.account_customizations_name}"
-}
+#   account_customizations_name = "${local.account_customizations_name}"
+# }
