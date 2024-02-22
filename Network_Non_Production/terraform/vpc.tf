e17899584d809cf5003882a34f2799913b18f388
Nitin Khaitan <khaitan.nitin@gmail.com>
2024-02-22 23:17:48 +1100

    vpc version updated

--- Network_Non_Production/terraform/vpc.tf
+++ Network_Non_Production/terraform/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc_network_non_production" {
   source  = "Codzs-Architecture/vpc-impl/aws"
-  version = "0.0.12"
+  version = "0.0.13"
 
   account_customizations_name = "${local.account_customizations_name}"
 }
