ef150239d12a87728b79f69b312e88ae4bd1e0c7
Nitin Khaitan <khaitan.nitin@gmail.com>
2024-02-23 14:14:43 +1100

    cleanup

--- Network_Non_Production/terraform/vpc.tf
+++ Network_Non_Production/terraform/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc_network_non_production" {
   source  = "Codzs-Architecture/vpc-impl/aws"
-  version = "0.0.15"
+  version = "0.0.16"
 
   account_customizations_name = "${local.account_customizations_name}"
 }
